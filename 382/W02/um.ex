5a0cbe2437d9365574f21171b83bf2da708968d2
ascogeno <rosengren.kyle@gmail.com>
2025-09-26 11:27:31 -0600

    added some elixir files

--- 382/W02/um.ex
+++ 382/W02/um.ex
@@ -1,1 +1,13 @@
+##In your editor, assign the result of a multiplication operation to a variable and add a constant to it in another variable.
+
+#Next up is Tuples:
+#In your editor, assign a tuple representing a person’s location ({City, Country}) and extract the city name.
+
+#Now for Lists:
+#In your editor, use pattern matching to retrieve the last element of a list by reversing it first.
+
+#And finally, List BIFs:
+#In your editor, apply Enum.at/2 to retrieve the third element in a list.
+
+
 IO.puts("Hello world from Elixir")
