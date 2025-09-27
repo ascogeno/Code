2a9bb5f76afacb0d740cc2fe3e9b5bcfbf29f73f
ascogeno <rosengren.kyle@gmail.com>
2025-09-26 19:46:35 -0600

    changed stuff

--- 382/W02/um.ex
+++ 382/W02/um.ex
@@ -1,13 +1,32 @@
+# Variables
 ##In your editor, assign the result of a multiplication operation to a variable and add a constant to it in another variable.
+preresult = 7 * 6
 
-#Next up is Tuples:
-#In your editor, assign a tuple representing a person’s location ({City, Country}) and extract the city name.
+result = preresult + 10
 
-#Now for Lists:
-#In your editor, use pattern matching to retrieve the last element of a list by reversing it first.
-
-#And finally, List BIFs:
-#In your editor, apply Enum.at/2 to retrieve the third element in a list.
+IO.puts("Preresult: #{preresult}")
+IO.puts("Postresult: #{result}")
 
 
-IO.puts("Hello world from Elixir")
+# Tuples
+##In your editor, assign a tuple representing a person’s location ({City, Country}) and extract the city name.
+location = {"Paris", "France"}
+
+{city, _country} = location
+IO.puts("Extracted City: #{city}")
+
+
+
+# Lists
+##In your editor, use pattern matching to retrieve the last element of a list by reversing it first.
+list = [1, 2, 3, 4, 5]
+
+[last_element | _rest] = Enum.reverse(list)
+IO.puts("Last element but its a lie cause i reversed the list so really this is the first item: #{last_element}")
+
+
+
+# List BIFs (Built-In Functions from Enum)
+##In your editor, apply Enum.at/2 to retrieve the third element in a list.
+third = Enum.at(list, 2)
+IO.puts("Third: #{third}")
