7c4880d781aba29aaa268ab21ce3f54390ab6460
ascogeno <rosengren.kyle@gmail.com>
2025-10-15 10:13:49 -0600

    syncing

--- cse381-course/Assignments/AlgorithmLib/DAGShortestPath.cs
+++ cse381-course/Assignments/AlgorithmLib/DAGShortestPath.cs
@@ -19,40 +19,44 @@ public static class DAGShortestPath
      */
     public static List<int> Sort(Graph g)
     {
-        Queue<int> q = new Queue<int>(); //need this for later
-        List<int> in_degree = new List<int>(); //the in-degree that the book mentions
-        for (int i = 0; i < g.Size(); i++) //allows us to loop through all the vertices in the graph (g.Size())
+        Stack<int> s = new Stack<int>(); // need this for later
+        List<int> in_degree = Enumerable.Repeat(0, g.Size()).ToList(); // the in-degree that the book mentions
+
+        for (int i = 0; i < g.Size(); i++) // allows us to loop through all the vertices in the graph (g.Size())
         {
-            foreach (var edge in g.Edges(i))//looks through each edge in the graph
+            foreach (var edge in g.Edges(i)) // looks through each edge in the graph
             {
-                in_degree[edge.DestId]++;//idk this was an optimization AI suggestion
-                                         //At this point, we're supposed to be incrementing the in-degree for each adjacent vertex.
-                                         //It's hard for me to tell if that's what this does
-            }
-            if (in_degree[i] == 0) // While we're looping through, we need to gather all the vertices that connect to nothing. Or, got through the last loop with 0
-            {
-                q.Enqueue(i);//this is what I needed the queue for
+                in_degree[edge.DestId]++; // At this point, we're supposed to be incrementing the in-degree for each adjacent vertex.
             }
         }
-        
-        List<int> sorted = new List<int>(); //now we make the sorted list, finally
-        while (q.Count>0)
-        {
-            int y = q.Dequeue();
-            sorted.Add(y); //adding these last few vertices, in order of when we got them earlier.
 
-            foreach(var edge in g.Edges(y)) //idk,  this parts a bit technical. another suggestion from Chat to get this working
+        for (int i = 0; i < g.Size(); i++) // once all in-degrees are computed, we can now gather all vertices with 0 in-degree
+        {
+            if (in_degree[i] == 0)
+            {
+                s.Push(i); // this is what I needed the stack for
+            }
+        }
+
+        List<int> sorted = new List<int>(); // now we make the sorted list, finally
+        while (s.Count > 0)
+        {
+            int y = s.Pop();
+            sorted.Add(y); // adding these last few vertices, in order of when we got them earlier.
+
+            foreach (var edge in g.Edges(y)) // this part adjusts the in-degrees for all adjacent vertices
             {
                 in_degree[edge.DestId]--;
                 if (in_degree[edge.DestId] == 0)
                 {
-                    q.Enqueue(edge.DestId);
+                    s.Push(edge.DestId); // push the next vertex onto the stack if ready
                 }
             }
         }
-        //we return the sorted list!
-        return sorted;
-    } 
+
+    // we return the sorted list!
+    return sorted; //btw, this function had to be rewritten when I used a queue first. stacks are cool tho, didn't mind too much
+}
 
     /* Find the shortest path from a starting vertex to all
      * vertices in a DAG.  This function will need to
@@ -67,10 +71,37 @@ public static class DAGShortestPath
      *     NOTE: The above two output lists should contain Graph.INF as needed
      */
     public static (List<int>, List<int>) ShortestPath(Graph g, int startVertex)
+{
+    List<int> l = Sort(g); // first, get the topologically sorted list of vertices from the graph
+
+    List<int> distance = Enumerable.Repeat(Graph.INF, g.Size()).ToList(); // distance table, starts as infinity
+    List<int> predecessor = Enumerable.Repeat(Graph.INF, g.Size()).ToList(); // predecessor table, also starts as infinity
+    distance[startVertex] = 0; // distance from start to itself is 0
+
+    for (int i = 0; i < l.Count; i++) // loop through vertices in topological order
     {
-        List<int> l = Sort(g);
-        
-        return (new List<int>(), new List<int>());
-    } 
+        int u = l[i];
+
+        if (distance[u] != Graph.INF) // only process if this vertex is reachable
+        {
+            foreach (var edge in g.Edges(u)) // look through all edges from this vertex
+            {
+                int v = edge.DestId;
+                int w = edge.Weight;
+
+                // relax the edge directly here, it deserves a break
+                if (distance[v] > distance[u] + w)
+                {
+                    distance[v] = distance[u] + w;
+                    predecessor[v] = u;
+                }
+            }
+        }
+    }
+
+    // return both lists together
+    return (distance, predecessor);
+}
+
     
 }
