578aba498a60c0312744bb852a165573fdd792aa
ascogeno <rosengren.kyle@gmail.com>
2025-10-20 20:34:47 -0600

    did another competensee

--- 382/W06/trie_al_run/lib/trie_al_run.ex
+++ 382/W06/trie_al_run/lib/trie_al_run.ex
@@ -22,21 +22,121 @@
 # Please test your code to see if it works.
 
 
-defmodule TrieAlRun do
-  @moduledoc """
-  Documentation for `TrieAlRun`.
-  """
+defmodule Trie do
 
-  @doc """
-  Hello world.
+  @end_marker :end
 
-  ## Examples
+  # ========== Public API ==========
 
-      iex> TrieAlRun.hello()
-      :world
+  def new(), do: %{}
 
-  """
-  def hello do
-    :world
+  def empty?(trie), do: map_size(trie) == 0
+
+  def add(trie, []), do: Map.put(trie, @end_marker, nil)
+
+  def add(trie, [h | t]) do
+    subtrie = Map.get(trie, h, %{})
+    updated = add(subtrie, t)
+    Map.put(trie, h, updated)
+  end
+
+  def contains?(trie, []), do: Map.has_key?(trie, @end_marker)
+
+  def contains?(trie, [h | t]) do
+    case Map.get(trie, h) do
+    nil -> false
+    subtrie -> contains?(subtrie, t)
+    end
+  end
+
+  def remove(trie, []), do: Map.delete(trie, @end_marker)
+
+  def remove(trie, [h | t]) do
+    case Map.get(trie, h) do
+      nil -> trie
+      subtrie ->
+        updated = remove(subtrie, t)
+        result =
+          if map_size(updated) == 0 and not Map.has_key?(trie, @end_marker) do
+            Map.delete(trie, h)
+          else
+            Map.put(trie, h, updated)
+          end
+
+        result
+    end
+  end
+
+  def to_list(trie), do: flatten_with_prefix([], trie)
+
+  def from_list([]), do: new()
+  def from_list([seq | rest]), do: add(from_list(rest), seq)
+
+  def prefix(trie, prefix_seq) do
+    case walk_down(prefix_seq, trie) do
+      nil -> []
+      subtrie -> flatten_with_prefix(prefix_seq, subtrie)
+    end
+  end
+
+  def map(trie, func) do
+    to_list(trie)
+    |> Enum.map(func)
+    |> from_list()
+  end
+
+  def merge(trie1, trie2) do
+    from_list(to_list(trie1) ++ to_list(trie2))
+  end
+
+  def remove_prefix(trie, prefix_seq) do
+    case walk_down(prefix_seq, trie) do
+      nil -> trie
+      _ -> remove_prefix_path(prefix_seq, trie)
+    end
+  end
+
+  def size(trie), do: length(to_list(trie))
+
+  # ========== Internal Helpers ==========
+
+  defp walk_down([], trie), do: trie
+
+  defp walk_down([h | t], trie) do
+    case Map.get(trie, h) do
+      nil -> nil
+      subtrie -> walk_down(t, subtrie)
+    end
+  end
+
+  defp remove_prefix_path([], _trie), do: %{}
+
+  defp remove_prefix_path([h | t], trie) do
+    case Map.get(trie, h) do
+      nil -> trie
+      subtrie ->
+        updated = remove_prefix_path(t, subtrie)
+        if map_size(updated) == 0 do
+          Map.delete(trie, h)
+        else
+          Map.put(trie, h, updated)
+        end
+    end
+  end
+
+  defp flatten_with_prefix(prefix, trie) do
+    base =
+      if Map.has_key?(trie, @end_marker) do
+        [prefix]
+      else
+        []
+      end
+
+    children =
+      trie
+      |> Enum.reject(fn {k, _} -> k == @end_marker end)
+      |> Enum.flat_map(fn {k, sub} -> flatten_with_prefix(prefix ++ [k], sub) end)
+
+    base ++ children
   end
 end
--- 382/W06/trie_al_run/test/trie_al_run_test.exs
+++ 382/W06/trie_al_run/test/trie_al_run_test.exs
@@ -1,8 +1,107 @@
+ExUnit.start()
+
 defmodule TrieAlRunTest do
   use ExUnit.Case
-  doctest TrieAlRun
+  alias Trie
 
-  test "greets the world" do
-    assert TrieAlRun.hello() == :world
+  test "add and contains?" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(c a t))
+      |> Trie.add(~w(c a r))
+      |> Trie.add(~w(d o g))
+      |> Trie.add(~w(c a))
+
+    assert Trie.contains?(trie, ~w(c a t)) == true
+    assert Trie.contains?(trie, ~w(c a r)) == true
+    assert Trie.contains?(trie, ~w(d o g)) == true
+    assert Trie.contains?(trie, ~w(c a)) == true
+    assert Trie.contains?(trie, ~w(c o w)) == false
+    assert Trie.contains?(trie, []) == false
+  end
+
+  test "to_list returns all sequences" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(c a t))
+      |> Trie.add(~w(c a r))
+      |> Trie.add(~w(d o g))
+      |> Trie.add(~w(c a))
+
+    expected = [
+      ~w(c a t),
+      ~w(c a r),
+      ~w(d o g),
+      ~w(c a)
+    ]
+
+    assert Enum.sort(Trie.to_list(trie)) == Enum.sort(expected)
+  end
+
+  test "prefix returns sequences with matching prefix" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(c a t))
+      |> Trie.add(~w(c a r))
+      |> Trie.add(~w(d o g))
+      |> Trie.add(~w(c a))
+
+    assert Enum.sort(Trie.prefix(trie, ~w(c a))) == Enum.sort([
+             ~w(c a t),
+             ~w(c a r),
+             ~w(c a)
+           ])
+
+    assert Trie.prefix(trie, ~w(d)) == [~w(d o g)]
+    assert Trie.prefix(trie, ~w(x)) == []
+  end
+
+  test "remove deletes only the exact sequence" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(c a t))
+      |> Trie.add(~w(c a r))
+      |> Trie.add(~w(d o g))
+      |> Trie.add(~w(c a))
+
+    updated = Trie.remove(trie, ~w(c a))
+
+    assert Trie.contains?(updated, ~w(c a)) == false
+    assert Trie.contains?(updated, ~w(c a t)) == true
+    assert Enum.sort(Trie.to_list(updated)) == Enum.sort([
+             ~w(c a t),
+             ~w(c a r),
+             ~w(d o g)
+           ])
+  end
+
+  test "remove_prefix deletes all sequences starting with prefix" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(c a t))
+      |> Trie.add(~w(c a r))
+      |> Trie.add(~w(d o g))
+
+    updated = Trie.remove_prefix(trie, ~w(c a))
+    assert Trie.to_list(updated) == [~w(d o g)]
+  end
+
+  test "size counts all complete sequences" do
+    trie =
+      Trie.new()
+      |> Trie.add(~w(a))
+      |> Trie.add(~w(a b))
+      |> Trie.add(~w(b))
+
+    assert Trie.size(trie) == 3
+
+    reduced = Trie.remove(trie, ~w(a))
+    assert Trie.size(reduced) == 2
+  end
+
+  test "from_list builds a trie from list of sequences" do
+    list = [~w(a), ~w(a b), ~w(b)]
+    trie = Trie.from_list(list)
+    assert Enum.sort(Trie.to_list(trie)) == Enum.sort(list)
   end
 end
