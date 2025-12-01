9749ab2e3925d353f9f6ea5d055dad7bed0df75d
ascogeno <rosengren.kyle@gmail.com>
2025-12-01 13:07:57 -0700

    syncing again

--- cse381-course/Assignments/AlgorithmLib/obj/Debug/net9.0/AlgorithmLib.AssemblyInfo.cs
+++ cse381-course/Assignments/AlgorithmLib/obj/Debug/net9.0/AlgorithmLib.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c99061da27fea50682cef06f7b35a372739209c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd5f3df2496de4eefa28dccbc08501ef4fa8b64b")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmLib")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
