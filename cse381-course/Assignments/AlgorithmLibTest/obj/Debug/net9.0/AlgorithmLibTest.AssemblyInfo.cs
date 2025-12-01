c99061da27fea50682cef06f7b35a372739209c2
ascogeno <rosengren.kyle@gmail.com>
2025-12-01 13:06:38 -0700

    syncing

--- cse381-course/Assignments/AlgorithmLibTest/obj/Debug/net9.0/AlgorithmLibTest.AssemblyInfo.cs
+++ cse381-course/Assignments/AlgorithmLibTest/obj/Debug/net9.0/AlgorithmLibTest.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AlgorithmLibTest")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3faf81e7c8da07e50fe7b41d759df7e07898d201")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+611dd7453bed31b23c20db7891c740fb02af9da9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AlgorithmLibTest")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AlgorithmLibTest")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
